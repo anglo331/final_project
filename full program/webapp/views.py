7c3da5fc66976d7cf93769cc75cd53016a61332d
anglo331 <angloyasser@gmail.com>
2024-03-04 00:04:02 +0200

    inital login page is done

--- full program/webapp/views.py
+++ full program/webapp/views.py
@@ -5,3 +5,8 @@ views = Blueprint("views",__name__)
 @views.route("/")
 def home():
     return render_template('home.html')
+
+
+@views.route("/login")
+def login():
+    return render_template('login.html')
